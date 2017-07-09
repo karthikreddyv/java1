d96b4f22384e9a2bb432720afd3671f492f7f9fb
karthikreddyv <karthik_vishweshwar@srmuniv.edu.in>
2017-07-09 14:58:11 +0530

    Update odd_or_even.java

--- odd_or_even.java
+++ odd_or_even.java
@@ -1,24 +1,22 @@
-
 import java.util.Scanner;
 class even_or_odd
 {
 	public static void main (String[] args) 
-	{
-		
+	{	
 		Scanner s = new Scanner(System.in);
 		System.out.println("enter value of a");
-	   int	a = s.nextInt();
+		int a = s.nextInt();
 		if(a%2 == 0)
 		{
-		System.out.println("number is even");
+			System.out.println("number is even");
 		}
 		else if(a%2 == 1)
 		{
-		System.out.println("number is odd");
+			System.out.println("number is odd");
 		}
 		else
 		{
-		System.out.println("number is neither even nor odd");
+			System.out.println("number is neither even nor odd");
 		}
 	}
 }
