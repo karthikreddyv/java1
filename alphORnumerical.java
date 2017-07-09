46e6a71f58eb3ae3e5d2e749db835e449dcc64d6
karthikreddyv <karthik_vishweshwar@srmuniv.edu.in>
2017-07-09 15:29:19 +0530

    Update and rename alpha numerical.java to alphORnumerical.java

---
+++ alphORnumerical.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class alphabet_or_number
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char alphabet;
+		Scanner s = new Scanner(System.in);
+		alphabet = s.next().charAt(0);
+		if 
+		(Character.isLetter(alphabet))
+		System.out.println("It is an alphabet");
+		else
+		System.out.println("It is a number");
+	}
+}
