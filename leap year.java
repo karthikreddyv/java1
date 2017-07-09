a53768f4eba8403e7217f98cbe50d6cd5d805773
karthikreddyv <karthik_vishweshwar@srmuniv.edu.in>
2017-07-09 15:37:26 +0530

    Add files via upload

---
+++ leap year.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int year;
+		Scanner s = new Scanner(System.in);
+		year=s.nextInt();
+		if (year%4==0)
+		System.out.println("leap year");
+		else
+		System.out.println("non year year");
+	}
+}
