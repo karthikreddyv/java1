06f968d16138228f9d85cd00f13659eaf01dcb9a
karthikreddyv <karthik_vishweshwar@srmuniv.edu.in>
2017-07-09 15:38:25 +0530

    Update leap year.java

--- leap year.java
+++ leap year.java
@@ -1,18 +1,16 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class leapyear
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args)
 	{
 		int year;
 		Scanner s = new Scanner(System.in);
 		year=s.nextInt();
-		if (year%4==0)
+		if (year %4 == 0)
 		System.out.println("leap year");
 		else
-		System.out.println("non year year");
+		System.out.println("non leap year");
 	}
 }
