3f90bf524282fca079a3d1ad0629bec31dd09b1e
karthikreddyv <karthik_vishweshwar@srmuniv.edu.in>
2017-07-09 15:44:00 +0530

    Update and rename Numbers.java to nuMBers.java

--- nuMBers.java
+++ nuMBers.java
@@ -1,11 +1,7 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+import java.util.Scanner;
+class numbers
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args)
 	{
 		int x=;
 		Scanner s = new Scanner(System.in);
