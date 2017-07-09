d147bddd494c4634d379b14ca980274d3acc2d8c
karthikreddyv <karthik_vishweshwar@srmuniv.edu.in>
2017-07-09 15:42:07 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int x=;
+		Scanner s = new Scanner(System.in);
+		x=s.nextInt();
+		if (x>0)
+		System.out.println("positive number");
+		else if (x<0)
+		System.out.println("negative number");
+		else
+		System.out.println("the given number is zero");
+		
+	}
+}
